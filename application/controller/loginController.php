c09e04247e1eee602cfb7b77f7f0481b8fc05d90
Marcoz Francel <f.marcoz@rt-iut.re>
2012-03-29 23:43:07 +0300

    Amelioration URL rewriting, modification des liens en consequence, deconnexion apres changement mot de passe

--- application/controller/loginController.php
+++ application/controller/loginController.php
@@ -9,12 +9,12 @@
 				foreach ($login as $key => $value) {
 					$_SESSION['login'][$key]=$value;
 				}
-				$url = HTTP_URL."/accueil/show";
+				$url = HTTP_URL."/accueil";
 				header('Location: '.$url);
 			}
 			else{
 				//Sinon on affiche une erreur
-				$url = HTTP_URL."/login/show";
+				$url = HTTP_URL."/login";
 				$time = 2000;
 				$html = "<div>Identifiants incorrects !</div>";
 				$html .= "<script>window.setTimeout(\"location=('".$url."');\",".$time.");</script>";
@@ -25,7 +25,7 @@
 		//Pour se déconnecter
 		case 'logout':
 			session_destroy();
-			$url = HTTP_URL."/login/show";
+			$url = HTTP_URL."/login";
 			$time = 2000;
 			$message = "Déconnexion réussi ! Redirection...";
 			$html =  "<div style='display:inline-block;'>".$message."</div>";
